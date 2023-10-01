b511762ec7e1081db1f99dc816a05ac7d963fdd4
Faruk Cagan <faruk.cagan@bestera.com.tr>
2023-10-01 13:52:07 +0300

    error page eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LoginPage from './pages/login-page/LoginPage';
 import UsersPage from './pages/users-page/UsersPage';
 import TodoPage from './pages/todo-page/Todo';
 import { store } from './pages/store/store';
+import ErrorPage from './pages/eror-page/ErrorPage';
 
 function App() {
 
@@ -14,6 +15,8 @@ function App() {
                     <Route path='/users' element={<UsersPage />} />
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/todos' element={<TodoPage />} />
+                    <Route path='/*' element={<ErrorPage />} />
+
                 </Routes>
             </Router>
         </Provider>
